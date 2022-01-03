d6b0fe82298542401a6f961c8c6536944f8c8652
AbdullahBasheer <abdullah.basheer402@gmail.com>
2022-01-03 04:10:21 -0800

    Assignment9

---
+++ script.js
@@ -0,0 +1,11 @@
+// function program1() {
+//   var dom=document;
+//   let array=["Abdullah ","Basheer","Roll no 4460"];
+//   let ali=dom.getElementById("program01")
+//   for (let index = 0; index < array.length; index++) 
+//   {
+//       console.log("Array Read" ,array[index],"Array Index",index);
+
+//   }
+//   program01.innerHTML = array;
+// }
